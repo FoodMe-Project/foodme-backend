5db916de630ca944e201d17b82183e5486d727b3
charlesjamb <charles.jambon@gmail.com>
2016-12-13 15:06:35 -0500

    removed const for app.listen

--- index.js
+++ index.js
@@ -121,8 +121,11 @@ app.post('/delete-recipe/', (req, res) => {
 })
 
 ///////////////////////////////////////////////////////////////////////////////
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
+// const server = app.listen(port, function () {
+//   console.log('Web Server is listening on port: ', port);
+// });
 
-const server = app.listen(port, function () {
-  console.log('Web Server is listening on port: ', port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
