efe4e0091fc40bf74bc73231461299c052234909
charlesjamb <charles.jambon@gmail.com>
2016-12-14 10:34:03 -0500

    trying the reconnect function

--- index.js
+++ index.js
@@ -17,42 +17,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Access to the database
-console.log(process.env.CLEARDB_DATABASE_URL);
+let db_config = process.env.CLEARDB_DATABASE_URL;
 
 let connection;
-if (process.env.CLEARDB_DATABASE_URL) {
-  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-}
-else { 
-  connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'sqltemppassword',
-    database : 'foodme'
-  });
-}
-
-// function handleDisconnect() {
-//   connection = mysql.createConnection({process.env.CLEARDB_DATABASE_URL});
-
-//   connection.connect(function(err) {
-//     if(err) {
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000);
-//     }
-//   });
-
-//   connection.on('error', function(err) {
-//     console.log('db error', err);
-//     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-//       handleDisconnect();
-//     } else {
-//       throw err;
-//     }
+// if (process.env.CLEARDB_DATABASE_URL) {
+//   connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+// }
+// else { 
+//   connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'sqltemppassword',
+//     database : 'foodme'
 //   });
 // }
 
-// handleDisconnect();
+function handleDisconnect() {
+  connection = mysql.createConnection({db_config});
+
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 const sqlAPI = foodme(connection);
 ///////////////////////////////////////////////////////////////////////////////
