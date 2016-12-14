8e0dcfbc8240d033484bbf8b6268433b4f7b567e
charlesjamb <charles.jambon@gmail.com>
2016-12-14 11:38:16 -0500

    more changes on handledisconnect

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+///////////////////////////////////////////////////////////////////////////////
 // Access to the database
-// let db_config = process.env.CLEARDB_DATABASE_URL;
-
 let connection;
 if (process.env.CLEARDB_DATABASE_URL) {
   connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
@@ -45,34 +44,12 @@ function handleDisconnect(conn) {
     console.log('Re-connecting lost connection: ' + err.stack);
 
     connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-    handleDisconnect(connection);
     connection.connect();
+    handleDisconnect(connection);
   });
 }
 
 handleDisconnect(connection);
-
-// function handleDisconnect() {
-//   connection = mysql.createConnection({db_config});
-
-//   connection.connect(function(err) {
-//     if(err) {
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000);
-//     }
-//   });
-
-//   connection.on('error', function(err) {
-//     console.log('db error', err);
-//     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-//       handleDisconnect();
-//     } else {
-//       throw err;
-//     }
-//   });
-// }
-
-// handleDisconnect();
 
 const sqlAPI = foodme(connection);
 ///////////////////////////////////////////////////////////////////////////////
