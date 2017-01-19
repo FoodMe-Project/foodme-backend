d18890bbc10a8f757633e36e34c3c89d8153b1de
charlesjamb <charles.jambon@gmail.com>
2017-01-19 19:08:33 +0100

    added start script

--- index.js
+++ index.js
@@ -143,6 +143,6 @@ app.post('/delete-recipe/', (req, res) => {
 })
 
 ///////////////////////////////////////////////////////////////////////////////
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3030, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
